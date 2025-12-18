ce673595e94cec9a2821c7864ba2bcbbe966e446
Monamie Aoshima <monamieaoshima@gmail.com>
2025-12-18 00:44:12 +0000

    cluster merging

--- src/compiler/qccd_color_qubits_to_ions.py
+++ src/compiler/qccd_color_qubits_to_ions.py
@@ -4,6 +4,7 @@ from typing import (
     Sequence,
     Tuple,
 )
+from collections import deque
 from src.utils.qccd_nodes import *
 from src.utils.qccd_operations import *
 from src.utils.qccd_operations_on_qubits import *
@@ -11,16 +12,10 @@ from src.utils.qccd_arch import *
 from src.compiler.qccd_parallelisation import *
 
 
-        
-
-
-def _TriangularPartitionIons(
-    ions: Sequence[Ion], coords: npt.NDArray[np.float64], trapCapacity: int, tiles: list, qubitIonsMap: dict
+def TriangularPartitionIons(
+    ions: Sequence[Ion], coords: npt.NDArray[np.float64], trapCapacity: int
 ) -> Sequence[Tuple[Sequence[Ion], npt.NDArray[np.float64]]]:
     
-    # think of new heuristic 
-    # try to cluster ions of same color together - cluster by shape first, then by color
-    # get extra context of which ions belong to which tiles 
     #ideally want to keep track of each triangle vertex regardless of which triangle it is in to make the maths easier
     # get vertices of outer triangle to use TriangleNode clustering
     A = np.array([np.min(coords[:,0]) - 1.0, np.min(coords[:,1]) - 1.0])
@@ -65,8 +60,8 @@ def _TriangularPartitionIons(
             dot20 = np.dot(pa,ba)
             dot21 = np.dot(pa,ca)
 
-            v = (dot11 * dot20 - dot01 * dot21) / denom
-            w = (dot00 * dot21 - dot01 * dot20) / denom
+            v = (dot11 * dot20 - dot01 * dot21) * idenom
+            w = (dot00 * dot21 - dot01 * dot20) * idenom
             u = 1.0 - v - w
 
             if u > 0.5:
@@ -82,10 +77,67 @@ def _TriangularPartitionIons(
         for i in ['top', 'left', 'right', 'center']:
             if buckets[i]:
                 stack.append((subtriangles[i], np.array(buckets[i]), depth + 1))
-        
-    return all_clusters
+    
+    result = np.empty(len(all_clusters), dtype=Tuple[Sequence[Ion], npt.NDArray[np.float64]])
+    for i in range(len(all_clusters)):
+        clust = all_clusters[i]
+        clusterCentre = np.mean(clust, axis=0)
+        result[i] = (clust, clusterCentre)
+    coordsToIons = {(c[0], c[1]): i for c, i in zip(coords, ions)}
+
+    return result
     # change to reduce overclustering - add some check to see if clusters can be merged together if under capacity
 
+def MergeUnderfilledClusters(
+    clusters: Sequence[Tuple[Sequence[Ion], npt.NDArray[np.float64]]], trapCapacity: int, coordsToIons: dict
+) -> Sequence[Tuple[Sequence[Ion], npt.NDArray[np.float64]]]:
+    
+    toCheck = []
+
+    for i, (coords, center) in enumerate(clusters):
+        toCheck.append({'size': len(coords), 'center': center, 'active': True, 'coords': coords})
+    
+    while True:
+        candidates = [t for t in toCheck if t['active']]
+        candidates.sort(lambda x: x[0])
+        merged = False
+
+        for i in range(len(candidates)):
+            c1 = candidates[i]
+            bestpartner = -1
+            mindist = float('inf')
+
+            for j in range(i+1, len(candidates)):
+                c2 = candidates[j]
+                dist = np.linalg.norm(c1[1] - c2[1])
+                if dist < mindist and c1[0]+c2[0] <= trapCapacity:
+                    mindist = dist
+                    bestpartner = j
+            if bestpartner != -1:
+                c2 = candidates[bestpartner]
+                
+                newsize = c1[0] + c2[0]
+                newcenter = (c1[0]*c1[1] + c2[0]*c2[1]) / newsize
+
+                newcluster = {'size': newsize, 'center': newcenter, 'active': True,
+                              'coords': c1['coords'] + c2['coords']}
+                
+                c1['active'] = False
+                c2['active'] = False
+                toCheck.append(newcluster)
+
+                merged = True
+                break
+        if not merged:
+            break
+    
+    finalcoords = [t['coords'] for t in toCheck if t['active']]
+    res = []
+    for clust in finalcoords:
+        ions = [coordsToIons[(c[0], c[1])] for c in clust] 
+        res.append(ions)
+    return res
+        
 def _ShapePartitionions(
     ions: Sequence[Ion], coords: npt.NDArray[np.float64], trapCapacity: int
 ) -> Sequence[Tuple[Sequence[Ion], npt.NDArray[np.float64]]]:
