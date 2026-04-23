1d921e171f7a7f9a828c12a763cd3741e87eda61
Monamie Aoshima <monamieaoshima@gmail.com>
2026-04-23 23:45:23 +0100

    opt routing num calcs

--- src/simulator/qccd_circuit.py
+++ src/simulator/qccd_circuit.py
@@ -487,15 +487,15 @@ class QCCDCircuit(stim.Circuit):
         instructions, barriers = self._parseCircuitString(dataQubitsIdxs=dataQubitsIdxs, measureQubitsIdxs=measureQubitsIdxs)
         if (trapCapacity-1) * ((rows-1) * (2*cols-1)+cols) < len(self._ionMapping):
             raise ValueError("processCircuit: not enough traps")
-        #clusters = regularPartition(self._measurementIons, self._dataIons, trapCapacity)
+        clusters = regularPartition(self._measurementIons, self._dataIons, trapCapacity)
         #print(f"clusters = {clusters}")
-        clusters = regularColorPartition_vectorised(
+        """ clusters = regularColorPartition_vectorised(
             self._measurementIons,
             self._dataIons,
             trapCapacity,
             merge_strategy=cluster_merge_strategy,
             merge_threshold=cluster_merge_threshold,
-        )
+        ) """
 
         #  temp cluster printing
         def visualize_clusters(clusters, trapCapacity):
